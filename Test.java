18d8caf11d33e5bb33c533c8c26babea31ad7569
janki <jankikumar2012@gmail.com>
2021-07-04 17:45:55 +0530

    change

--- Test.java
+++ Test.java
@@ -1,7 +1,17 @@
 class Test {
 	public static void main(String args[]){
 		System.out.println("Hello World welcome  to gaya");
-		System.out.println("Hello Worl");
+		//System.out.println("Hello Worl");
 		System.out.println("Hello World wel");
 	}
 }
+
+
+
+git clone
+
+git status
+git add
+git commit
+git merge
+git branch -d abc
